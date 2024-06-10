a7b7aa86967e2277005620e15b4e2af160e04ba1
D-VoraSarvadhi <135818808+D-VoraSarvadhi@users.noreply.github.com>
2024-06-10 11:02:30 +0530

    Implement stripe key

---
+++ dist/stripe/stripeConfig.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const stripe_1 = __importDefault(require("stripe"));
+const stripeConfig = (secretKey) => {
+    return new stripe_1.default(secretKey, {
+        apiVersion: '2024-04-10'
+    });
+};
+exports.default = stripeConfig;
