09e3f7d5ee112eeaedc98ee9c4edc8cfa453fbb0
D-VoraSarvadhi <135818808+D-VoraSarvadhi@users.noreply.github.com>
2024-06-10 12:56:53 +0530

    Change in stripe interface

--- dist/stripe/stripeConfig.d.ts
+++ dist/stripe/stripeConfig.d.ts
@@ -1,3 +1,6 @@
-import { StripeConfig } from '../types/index';
+import Stripe from "stripe";
+interface StripeConfig {
+    (secretKey: string): Stripe;
+}
 declare const stripeConfig: StripeConfig;
 export default stripeConfig;
